08522de91223e09c8a20d24749fd66d7415e74d7
Yahav <109096266+YahavAvramov@users.noreply.github.com>
2023-01-08 11:04:53 +0200

    first commit

--- Airport-server/airport-server/Program.cs
+++ Airport-server/airport-server/Program.cs
@@ -6,10 +6,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextPool<AirportContext>(options => options.UseSqlServer("name=ConnectionStrings:MyAirportConnectionString"));
